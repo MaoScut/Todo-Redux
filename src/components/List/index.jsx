a7fa9ff95faf604a176751fa45ab4f77a5232a34
MaoScut <849345372@qq.com>
2017-08-20 00:59:34 +0800

    原先因为点击checkbox会触发异步acition，造成延迟，现在把TodoItem改为有状态的组件，点击后，先setState，再回调触发action

--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Statics from '../Statics';
 
-function List({ items, hideHandle, hideDone, editHandle }) {
+function List({ items, hideHandle, hideDone, editHandle, checkHandle }) {
+  let doneNum = 0;
   const list = items.map((item) => {
     const { id, content, checked } = item;
+    if (checked) doneNum += 1;
     return (
       <TodoItem
         key={id}
@@ -12,15 +15,18 @@ function List({ items, hideHandle, hideDone, editHandle }) {
         checked={checked}
         hideDone={hideDone}
         editHandle={editHandle}
+        checkHandle={checkHandle}
       />
     );
   });
   const buttonText = hideDone ? '显示已完成事项' : '隐藏已完成事项';
+  const donePercent = String(doneNum).concat('/', items.length);
   return (
     <div>
       <ul>
         {list}
       </ul>
+      <Statics donePercent={donePercent} />
       <button onClick={() => hideHandle()}>{buttonText}</button>
     </div>
   );
@@ -37,26 +43,88 @@ List.propTypes = {
   hideDone: PropTypes.bool.isRequired,
   hideHandle: PropTypes.func.isRequired,
   editHandle: PropTypes.func.isRequired,
+  checkHandle: PropTypes.func.isRequired,
 };
 
-function TodoItem({ content, id, checked, hideDone, editHandle }) {
-  const className = checked && hideDone ? 'hidden' : '';
-  function onEditClick(e) {
-    e.stopPropagation();
-    editHandle({
-      id,
+// function TodoItem({ content, id, checked, hideDone, editHandle, checkHandle }) {
+//   const className = checked && hideDone ? 'hidden' : '';
+//   let checkbox = null;
+//   function onEditClick(e) {
+//     e.stopPropagation();
+//     editHandle({
+//       id,
+//       content,
+//       checked,
+//     });
+//   }
+//   function checkboxClick() {
+//     // checkbox.checked = !checkbox.checked;
+//     checkHandle(id);
+//   }
+//   return (
+//     <li className={className}>
+//       <input
+//         type="checkbox"
+//         className="checkbox"
+//         value={id}
+//         checked={checked}
+//         onClick={checkboxClick}
+//         ref={(input) => { checkbox = input; }}
+//       />
+//       {content}
+//       <button className="edit" onClick={onEditClick}>编辑</button>
+//       <button className="delete-btn" value={id}>删除</button>
+//     </li>
+//   );
+// }
+class TodoItem extends React.Component {
+  constructor(props) {
+    super(props);
+    const { id, content, checked, hideDone } = props;
+    this.state = {
       content,
+      id,
       checked,
+      hideDone,
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+  // componentDidUpdate() {
+  //   // this.props.checkHandle(this.props.id); dangerous!
+  // }
+  onEditClick(e) {
+    e.stopPropagation();
+    this.props.editHandle({
+      id: this.props.id,
+      content: this.props.content,
+      checked: this.props.checked,
     });
   }
-  return (
-    <li className={className}>
-      <input type="checkbox" className="checkbox" value={id} checked={checked} readOnly />
-      {content}
-      <button className="edit" onClick={onEditClick}>编辑</button>
-      <button className="delete-btn" value={id}>删除</button>
-    </li>
-  );
+  handleClick() {
+    this.setState(preState => ({
+      checked: !preState.checked,
+    }), () => {
+      this.props.checkHandle(this.props.id);
+    });
+  }
+  render() {
+    const { hideDone, content, id } = this.props;
+    const className = hideDone ? 'hidden' : '';
+    return (
+      <li className={className}>
+        <input
+          type="checkbox"
+          className="checkbox"
+          value={id}
+          checked={this.state.checked}
+          onChange={this.handleClick}
+        />
+        {content}
+        <button className="edit" onClick={this.onEditClick}>编辑</button>
+        <button className="delete-btn" value={id}>删除</button>
+      </li>
+    );
+  }
 }
 TodoItem.propTypes = {
   content: PropTypes.string.isRequired,
@@ -64,6 +132,7 @@ TodoItem.propTypes = {
   checked: PropTypes.bool.isRequired,
   hideDone: PropTypes.bool.isRequired,
   editHandle: PropTypes.func.isRequired,
+  checkHandle: PropTypes.func.isRequired,
 };
 
 export default List;
