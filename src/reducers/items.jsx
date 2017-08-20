568b5e8df53c6677a398584cd3b50226134920bc
MaoScut <849345372@qq.com>
2017-08-20 18:51:44 +0800

    check不需要改变state，所以直接转到default

--- src/reducers/items.jsx
+++ src/reducers/items.jsx
@@ -5,6 +5,7 @@ export default function (state = initialState, action) {
   switch (action.type) {
     case ActionTypes.GET_ITEMS:
       return action.items.slice();
+    // case ActionTypes.CHECK:
     default:
       return state;
   }
