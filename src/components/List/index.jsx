bbcbe479a213ec75122b64f8c291b5169dff3133
MaoScut <849345372@qq.com>
2017-08-20 18:49:25 +0800

    后代的点击事件都委托给list来操作

--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -33,6 +33,7 @@ import TodoItem from '../TodoItem';
 //   );
 // }
 //  bubble list
+// 传入items，事件
 class List extends React.Component {
   constructor(props) {
     super(props);
@@ -52,33 +53,28 @@ class List extends React.Component {
     });
   }
   componentDidUpdate() {
-    const { items, clickItemId } = this.state;
+    // const { items, clickItemId } = this.state;
     // const item = items.find(v => v.id === clickItemId);
     // if (clickItemId) this.props.checkHandle(clickItemId);
   }
-  shouldComponentUpdate(nextProps, nextState) {
-    // 什么时候不用更新呢？
-    // 如果这一次从store传过来的不等于自己的state，好像没办法做到？？
-    // 记录一下具体问题
-    // 假设现在操作目标是一个没有勾选的box，一些事件的触发顺序
-    // 捕捉到box的点击，调用自己的setState，改变完自己的state后，
-    // 一小段时间后，分派action去修改store，store数据改变，再次触发setState，因为这两个数据一致，看起来就像没更新
-    // 如果是两次点击的话，自己状态复原后，会收到第一次点击分派的action导致的数据更新，状态不一致，改变了
-    // 然后再收到第二次点击分派的action，才变回来
-    // 所以会闪一下
-  }
+  // shouldComponentUpdate(nextProps, nextState) {
+  // 什么时候不用更新呢？
+  // 如果这一次从store传过来的不等于自己的state，好像没办法做到？？
+  // 记录一下具体问题
+  // 假设现在操作目标是一个没有勾选的box，一些事件的触发顺序
+  // 捕捉到box的点击，调用自己的setState，改变完自己的state后，
+  // 一小段时间后，分派action去修改store，store数据改变，再次触发setState，因为这两个数据一致，看起来就像没更新
+  // 如果是两次点击的话，自己状态复原后，会收到第一次点击分派的action导致的数据更新，状态不一致，改变了
+  // 然后再收到第二次点击分派的action，才变回来
+  // 所以会闪一下
+  // 点一个checkbox就触发一个action这个代价太高了吧。。
+  // }
   captureBubble(e) {
-    // const className = e.target.className;
-    // switch (className) {
-    //   case 'delete-btn': this.props.deleteHandle(e.target.value); break;
-    //   case 'checkbox': this.props.checkHandle(e.target.value); break;
-    //   case 'edit-btn': this.props.editHandle(this.state.items.find(v => v.id === e.target.value)); break;
-    //   default:
-    // }
     e.stopPropagation();
-    if (e.target.className === 'checkbox') {
-      // 在setState中别用e
-      const targetId = e.target.value;
+    const className = e.target.className;
+    const targetId = e.target.value;
+    if (className.includes('checkbox')) {
+      // 在setState中别用e     
       this.setState((preState) => {
         const index = preState.items.findIndex(v => v.id === targetId);
         const newItems = preState.items.slice();
@@ -89,10 +85,20 @@ class List extends React.Component {
         };
       }, () => this.props.checkHandle(targetId));
     }
+    if (className.includes('edit')) {
+      const item = this.state.items.find(v => v.id === targetId);
+      this.props.editHandle(item);
+    }
+    if (className.includes('delete')) {
+      this.setState(preState => ({
+        items: preState.items.filter(v => v.id !== targetId),
+      }), () => this.props.deleteHandle(targetId));
+    }
     // 缺陷：虽然可以马上响应改变，但是如果没勾选，然后连续点击两次，会闪一下勾选的状态，再变会没勾选
   }
-  toggleBtnHandleClick() {
-    this.setState(preState => this.setState({
+  toggleBtnHandleClick(e) {
+    e.stopPropagation();
+    this.setState(preState => ({
       hideDone: !preState.hideDone,
     }));
   }
@@ -129,7 +135,7 @@ List.propTypes = {
     content: PropTypes.string,
   })).isRequired,
   hideDone: PropTypes.bool.isRequired,
-  hideHandle: PropTypes.func.isRequired,
+  // hideHandle: PropTypes.func.isRequired,
   editHandle: PropTypes.func.isRequired,
   checkHandle: PropTypes.func.isRequired,
   deleteHandle: PropTypes.func.isRequired,
