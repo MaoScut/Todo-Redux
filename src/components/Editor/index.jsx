b775551fc17df701b0152baf865dc8f71cc5e744
MaoScut <849345372@qq.com>
2017-08-20 23:46:05 +0800

    添加简单样式

--- src/components/Editor/index.jsx
+++ src/components/Editor/index.jsx
@@ -12,14 +12,17 @@ function Editor({ item, onSave, onCancel }) {
     }
   }
   return (
-    <div>
-      <input
-        type="text"
-        ref={(input) => { updateInput = input; }}
-        defaultValue={item ? item.content : ''}
-      />{item ? '输入修改' : '输入待办事项'}
-      <button onClick={save}>{item ? '更新' : '保存'}</button>
-      <button onClick={onCancel}>取消</button>
+    <div className="editor-container">
+      <div className="cover" />
+      <div className="editor">
+        <input
+          type="text"
+          ref={(input) => { updateInput = input; }}
+          defaultValue={item ? item.content : ''}
+        />{item ? '输入修改' : '输入待办事项'}
+        <button onClick={save}>{item ? '更新' : '保存'}</button>
+        <button onClick={onCancel}>取消</button>
+      </div>
     </div>
   );
 }
