5522aa961e7e2435d25fab85825d3c6ef3399583
MaoScut <849345372@qq.com>
2017-08-24 00:25:44 +0800

    使用纯函数作为actionCreator

--- src/components/CTodo/index.jsx
+++ src/components/CTodo/index.jsx
@@ -1,13 +1,19 @@
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import App from '../Todo';
-import * as actionCreators from '../../actions';
+// import * as actionCreators from '../../actions';
+import * as actionCreators from '../../actions/pureAction';
+// import { fetchItems } from '../../sagas';
 
 export default connect(
   state => state,
   dispatch => ({
     actions: bindActionCreators(actionCreators, dispatch),
+    // test: bindActionCreators({fetchItems}, dispatch),
   }),
+  // {
+  //   ...actionCreators,
+  // },
 )(App);
 // es7的修饰器，据说发生在编译阶段，然而要注意导出模块的语法
 // @ connect(
