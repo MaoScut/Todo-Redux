47486e86ceae2e1aedb2a529bfb91530ccb05578
MaoScut <849345372@qq.com>
2017-08-24 00:27:34 +0800

    删除无接受数据的action的处理

--- src/reducers/items.jsx
+++ src/reducers/items.jsx
@@ -13,14 +13,19 @@ const initialState = [];
 // }
 
 const reducer = handleActions({
-  [ActionTypes.GET_ITEMS](state, action) {
+  // [ActionTypes.GET_ITEMS](state, action) {
+  // 如果是用redux-thunk或者是redux-promise， 数据在这里取得， 
+  // 如果是saga的话，那么在receive items的action中获取
+  // return action.payload;
+  // },
+  [ActionTypes.RECEIVE_ITEMS](state, action) {
     return action.payload;
   },
-  [ActionTypes.SAVE](state, action) {
-    return action.payload;
-  },
-  [ActionTypes.DELETE](state, action) {
-    return action.payload;
-  },
+  // [ActionTypes.SAVE](state, action) {
+  //   return action.payload;
+  // },
+  // [ActionTypes.DELETE](state, action) {
+  //   return action.payload;
+  // },
 }, initialState);
 export default reducer;
